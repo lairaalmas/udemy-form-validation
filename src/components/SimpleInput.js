752c801f5ef30cd063d51d91bffe66dc8d7e5888
lairaalmas <lairaalmas@gmail.com>
2023-02-07 15:33:36 -0300

    refactoring code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,48 +2,31 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  // if field was invalid on blur && user enters new value
-  // remove error message
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
-
-    if (e.target.value.trim() !== "") {
-      setEnteredNameIsValid(true);
-    }
   };
 
-  // if field is empty on blur
-  // dispplay error message
   const nameInputBlurHandler = (e) => {
     setEnteredNameTouched(true);
-
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-    }
   };
 
-  // if field is invalid on submit
-  // display error message
   const formSubmitionHandler = (e) => {
     e.preventDefault();
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-      return;
-    }
-
-    setEnteredNameIsValid(true);
+    if (!enteredNameIsValid) return;
 
     console.log(enteredName);
-    setEnteredName("");
-  };
 
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+    setEnteredName("");
+    setEnteredNameTouched(false);
+  };
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
